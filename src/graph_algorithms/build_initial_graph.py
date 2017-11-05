345bfab564049f55262dafc7756ce7fc2718838a
Isaac Scheinfeld <ischeinfeld@outlook.com>
2017-11-05 10:17:45 -0800

    Added class for checking boundary inclusion.

--- src/graph_algorithms/build_initial_graph.py
+++ src/graph_algorithms/build_initial_graph.py
@@ -6,12 +6,12 @@ import numpy as np
 from utility_classes import Location
 from utility_classes import Obstacle
 
-def build_initial_graph(flight_boundary: List[Location], stationary_obstacles: List[Obstacle],
+def build_initial_graph(boundary: List[Location], stat_obstacles: List[Obstacle],
                         granularity: float) -> nx.Graph:
     """Builds a space-filling graph within flight boundaries and removes nodes in obstacles.
     Args:
-        flight_boundary: the gps locations of the polygon defining the flight boundary
-        stationary_obstacles: the locations and sizes of the stationary obstacles
+        boundary: the gps locations of the polygon defining the flight boundary
+        stat_obstacles: the locations and sizes of the stationary obstacles
         granularity: the distance between graph nodes (TODO in what measure?)
     Returns:
         graph: a graph filling the space, whose nodes are Locations
@@ -19,8 +19,8 @@ def build_initial_graph(flight_boundary: List[Location], stationary_obstacles: L
     graph = nx.Graph()
 
     # TODO replace with space filling graph
-    graph.add_nodes_from(flight_boundary)
-    for pair in zip(flight_boundary, flight_boundary[1:] + flight_boundary[0:1]):
+    graph.add_nodes_from(boundary)
+    for pair in zip(boundary, boundary[1:] + boundary[0:1]):
         graph.add_edge(pair[0], pair[1])
 
     # TODO remove nodes in obstacles
@@ -29,9 +29,9 @@ def build_initial_graph(flight_boundary: List[Location], stationary_obstacles: L
 
 # test if run as main
 if __name__ == "__main__":
-    flight_boundary = [Location(0.0, 0.0), Location(1.1, 0.0), Location(1.1, 1.1)]
-    stationary_obstacles = []
+    boundary = [Location(0.0, 0.0), Location(1.1, 0.0), Location(1.1, 1.1)]
+    stat_obstacles = []
     granularity = 0.0
 
-    initial_graph = build_initial_graph(flight_boundary, stationary_obstacles, granularity)
+    initial_graph = build_initial_graph(boundary, stat_obstacles, granularity)
     print(list(initial_graph.edges()))
---
+++ src/graph_algorithms/polygon.py
@@ -0,0 +1,67 @@
+from typing import List
+from typing import Tuple
+
+import numpy as np
+
+from utility_classes import Location
+
+class Polygon:
+    """A class that represents the flight boundary and can check point containment
+    See http://alienryderflex.com/polygon/ for details.
+    """
+
+    def __init__(self, boundary: List[Location]):
+        """Precalculates values for checking polygon point-inclusion.
+        Args:
+            boundary: a list of boundary point locations
+        """
+        self.corner_count = len(boundary)
+        self.poly_x = np.array([p.lon for p in boundary])
+        self.poly_y = np.array([p.lat for p in boundary])
+        self.constant = np.zeros(self.poly_x.size)
+        self.multiple = np.zeros(self.poly_x.size)
+        
+        j = self.corner_count - 1
+    
+        for i in range(self.corner_count):
+            if self.poly_y[i] == self.poly_y[j]:
+                self.constant[i] = self.poly_x[i]
+                self.multiple[i] = 0
+            else:
+                self.constant[i] = (self.poly_x[i]
+                                    - (self.poly_y[i] * self.poly_x[j])
+                                       / (self.poly_y[j] - self.poly_y[i])
+                                    + (self.poly_y[i] * self.poly_x[i])
+                                       / (self.poly_y[j] - self.poly_y[i]))
+                self.multiple[i] = ((self.poly_x[j] - self.poly_x[i])
+                                    / (self.poly_y[j] - self.poly_y[i]))
+            j = i
+
+    def __contains__(self, point: Location) -> bool:
+        """Checks whether a point is in the polygon
+        Args:
+            point: a location
+        Returns:
+            odd_nodes: true iff point in polygon
+        """
+
+        x = point.lon
+        y = point.lat
+        j = self.corner_count - 1
+        odd_nodes = False
+
+        for i in range(self.corner_count):
+            if ((self.poly_y[i] < y and self.poly_y[j] >= y)
+                or (self.poly_y[j] < y and self.poly_y[i] >= y)):
+                odd_nodes ^= (y * self.multiple[i] + self.constant[i] < x)
+            j = i
+
+        return odd_nodes
+
+
+# test if run as main
+if __name__ == "__main__":
+    boundary = [Location(0.0, 0.0), Location(1.1, 0.0), Location(1.1, 1.1)]
+    boundary_poly = Polygon(boundary)
+    point = Location(0.5, 1.2)
+    print(point in boundary_poly)
