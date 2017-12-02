d19f53c2f5dd9b268bee1df58e5f6f5a154887d4
Isaac Scheinfeld <ischeinfeld@outlook.com>
2017-12-02 13:28:58 -0800

    Started benchmark code

---
+++ benchmarks/point_to_point/problem_definition.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""problem.py
+Defines the problem of point to point navigation. To be loaded before training model.
+"""
+
+area = ((-50.0, -50.0), (150.0, 150.0)) # ((x_min, y_min), (x_max, y_max))
+waypoints = ((0.0, 0.0), (100.0, 100.0)) # ((x_start, y_start), (x_end, y_end))
+max_obstacle_count = 5 # Each environment will have 1 - N obstacles, with equal probability
+obstacle_radius_mean = 5 # Mean of obstacle radius
+obstacle_radius_variance = 2 # Variance of obstacle radius
