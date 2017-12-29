24c50d8a4804c2a3a2daf73ecb4e485292a556b5
Isaac Scheinfeld <ischeinfeld@outlook.com>
2017-12-29 09:27:25 -0500

    Updated Test Framework

--- tests/test_context.py
+++ tests/test_context.py
@@ -1,9 +1,6 @@
 from . import context
 
 import sys
-import numpy as np
-import matplotlib.pyplot as plt
-
 import nav
 
 def test_nav_import():
--- tests/test_run.py
+++ tests/test_run.py
@@ -2,23 +2,34 @@ from . import context
 
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import shutil
 
 import nav.env
 import nav.plan
 
-# set parameters
-CONFIG_DIR = './tests/config/'
-boundary = np.genfromtxt(CONFIG_DIR + 'boundary.csv', delimiter=',').T
-static_obs = np.genfromtxt(CONFIG_DIR + 'static_obs.csv', delimiter=',').T
+RESULTS_DIR = 'tests/results/'
+CONFIG_DIR = 'tests/config/'
+BOUNDARY = np.genfromtxt(CONFIG_DIR + 'boundary.csv', delimiter=',').T
+STATIC_OBS = np.genfromtxt(CONFIG_DIR + 'static_obs.csv', delimiter=',').T
+ENV_PARAMS = {"granularity": 100}
+PLANNER_PARAMS = {}
 
-env_params = {"granularity": 100}
-planner_params = {}
+def test_environment_display():
+    """ tests a simple environment display script for exceptions
+    """
+    result_dir = RESULTS_DIR + 'test_environment_display/'
+    shutil.rmtree(result_dir, ignore_errors=True)
+    os.makedirs(result_dir)
 
-env = nav.env.Environment(boundary, static_obs, env_params)
-planner = nav.plan.Planner(env, planner_params)
+    env = nav.env.Environment(BOUNDARY, STATIC_OBS, ENV_PARAMS)
+    #planner = nav.plan.Planner(env, PLANNER_PARAMS)
+    
+    fig = plt.figure(figsize=(7,7))
+    ax = fig.add_subplot(111)
+    env.display(ax)
+    fig.savefig(result_dir + 'env')
 
-fig = plt.figure(figsize=(7,7))
-ax = fig.add_subplot(111)
-env.display(ax)
-plt.show()
-print(env)
+def test_planner_display():
+    """ tests a simple planner display script for exceptions
+    """
